eb6e2b37b6f77599ee29f2d6c3dfb859bf3f7d00
John Prince <jtprince@gmail.com>
2011-03-30 16:37:06 -0600

    version 0.0.1

--- Rakefile
+++ Rakefile
@@ -5,18 +5,18 @@ require 'jeweler'
 Jeweler::Tasks.new do |gem|
   # gem is a Gem::Specification... see http://docs.rubygems.org/read/chapter/20 for more options
   gem.name = "ms-quant"
-  gem.homepage = "http://github.com/jtprince/ms-quant"
+  gem.homepage = "http://github.com/princelab/ms-quant"
   gem.license = "MIT"
-  gem.summary = %Q{TODO: one-line summary of your gem}
-  gem.description = %Q{TODO: longer description of your gem}
+  gem.summary = %Q{quantitation of mass spectrometry datasets (proteomic, metabolomic/lipidomic)}
+  gem.description = %Q{quantitation of mass spectrometry datasets (proteomic, metabolomic/lipidomic).  Not related to MSQuant}
   gem.email = "jtprince@gmail.com"
-  gem.authors = ["John Prince"]
+  gem.authors = ["John T. Prince"]
   # Include your dependencies below. Runtime dependencies are required when using your gem,
   # and development dependencies are only needed for development (ie running rake tasks, tests, etc)
   #  gem.add_runtime_dependency 'jabber4r', '> 0.1'
   #  gem.add_development_dependency 'rspec', '> 1.2.3'
-  gem.add_development_dependency "bacon", ">= 0"
-  gem.add_development_dependency "bundler", "~> 1.0.0"
+  gem.add_runtime_dependency 'ms-ident', ">= 0.0.19"
+  gem.add_development_dependency "spec-more", ">= 0"
   gem.add_development_dependency "jeweler", "~> 1.5.2"
   gem.add_development_dependency "rcov", ">= 0"
 end
--- bin/peptide_hit_qvalues_to_spectral_counts_table.rb
+++ bin/peptide_hit_qvalues_to_spectral_counts_table.rb
@@ -7,10 +7,18 @@ require 'ms/quant/spectral_counts'
 
 require 'trollop'
 
+def putsv(*args)
+  if $VERBOSE
+    puts(*args) ; $stdout.flush
+  end
+end
+
 opts = Trollop::Parser.new do
   banner %Q{usage: #{File.basename(__FILE__)} peptide_centric_db.yml, file1.psq ...
 }
   opt :names, "array of names for the table (otherwise filenames)", :type => String
+  opt :fdr_percent, "%FDR as cutoff", :default => 1.0
+  opt :write_subset, "(development) write subset db", :default => false
 end
 
 opt = opts.parse(ARGV)
@@ -27,41 +35,92 @@ opt[:names] ||= ARGV.map do |file|
   base
 end
 
-start=Time.now
-ar_of_peptide_hit_ars = Ms::Ident::Peptide::Db::IO.open(peptide_centric_db_file) do |peptide_to_proteins|
-  puts "#{Time.now-start} seconds to read #{peptide_centric_db_file}" ; $stdout.flush
+class Ms::Ident::PeptideHit
+  attr_accessor :experiment_name
+end
+fdr_cutoff = opt[:fdr_percent] / 100
 
-  hit_to_exp = {}
+start=Time.now
+
+$VERBOSE = true
+
+ar_of_peptide_hit_ars = Ms::Ident::Peptide::Db::IO.open(peptide_centric_db_file) do |peptide_to_proteins|
+  putsv "#{Time.now-start} seconds to read #{peptide_centric_db_file}"
   ARGV.zip(opt[:names]).map do |file,exp| 
     peptide_hits = Ms::Ident::PeptideHit::Qvalue.from_file(file)
+    putsv "#{file}: #{peptide_hits.size} hits"
     peptide_hits.select! do |hit|
-      # update each peptide with its protein hits
-      hit.proteins = peptide_to_proteins[hit].map {|id| Ms::Ident::ProteinHit.new(id) }
+      if hit.qvalue <= fdr_cutoff
+        # update each peptide with its protein hits
+        prot_ids = peptide_to_proteins[hit.aaseq]
+        if prot_ids
+          hit.experiment_name = exp
+          hit.proteins = prot_ids
+        else ; false end
+      else
+        false
+      end
     end
-    peptide_hits.each {|h| hit_to_exp[hit] = exp }
     peptide_hits
   end
 end
 
+if opt[:write_subset] 
+  aaseqs_to_prots = {} 
+  ar_of_peptide_hit_ars.each do |pephits|
+    pephits.each do |pephit|
+      aaseqs_to_prots[pephit.aaseq] = pephit.proteins
+    end
+  end
+  outfile = "peptidecentric_subset.yml"
+  puts "writing #{outfile} with #{aaseqs_to_prots.size} aaseq->protids"
+  File.open(outfile,'w') do |out| 
+    aaseqs_to_prots.each do |k,v| 
+      out.puts(%Q{#{k}: #{v.map(&:id).join("\t") }}) 
+    end
+  end
+end
+
+$VERBOSE = true
+if $VERBOSE
+  opt[:names].zip(ar_of_peptide_hit_ars) do |name, pep_ar|
+    puts "#{name}: #{pep_ar.size}"
+  end
+end
+
 all_peptide_hits = ar_of_peptide_hit_ars.flatten(1)
+
+
+# because peptide_hit#proteins yields id strings (which hash properly),
+# each protein group is an array of 
 protein_groups = Ms::Ident::ProteinGroup.peptide_hits_to_protein_groups(all_peptide_hits)
+
+pephit_to_protein_groups = Hash.new {|h,k| h[k] = [] }
+protein_groups.each do |protein_group|
+  protein_group.peptide_hits.each {|hit| pephit_to_protein_groups[hit] << protein_group }
+end
+
+# partition them all out by filename
 
 ar_of_count_data = opt[:names].map do |name|
   pep_hit_to_prot_groups = Hash.new {|h,k| h[k] = [] }
   groups_of_pephits = protein_groups.map do |prot_group|
-    pep_hits = peptide_hits.select {|hit| hit_to_exp[hit] == name }
+    pep_hits = prot_group.peptide_hits.select {|hit| hit.experiment_name == name }
     pep_hits.each do |pep_hit|
       pep_hit_to_prot_groups[pep_hit] << prot_group
     end # returns the group of pep_hits
   end
-  counts = Ms::Quant::SpectralCounts.counts(groups_of_pephits, pep_hit_to_prot_groups)
+  counts = Ms::Quant::SpectralCounts.counts(groups_of_pephits) # do |pephit|
+  #  pephit_to_protein_groups[pephit].size
+  #end
 end
 
 # protein_groups
 # [ ar_of_counts_for_exp1, ar_of_counts_for_exp2, ar_of_counts_for_exp3 ]
 
 protein_groups.zip(*ar_of_count_data) do |row|
-  p row
+  pg = row.shift
+  puts (row.map(&:to_a).flatten + pg.to_a).join("\t")
 end
 
 
